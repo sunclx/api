94591865acb28425b35b4eb1e4e2f0d7bb61d519
sunclx <sunclx@gmail.com>
2016-02-03 18:41:12 +0800

    bug fix

--- tests/tests.go
+++ tests/tests.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 )
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":80", nil)
 }
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", r.URL.RequestURI())
+	name, err := url.QueryUnescape(r.URL.RequestURI()[1:])
+	if err != nil {
+		fmt.Fprintf(w, "failed: %v", err)
+	}
+	fmt.Fprintf(w, "Hello, %s!", name)
 }
